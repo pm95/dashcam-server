16ac63cb41667c2862a041615b72b1cd8e0e964e
Pietro Malky <pietromalky@gmail.com>
2019-11-17 15:03:40 -0600

    video uploading works

--- src/Server.py
+++ src/Server.py
@@ -1,12 +1,11 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, session
+from flask import Flask, flash, request, redirect, url_for, session, jsonify
 from werkzeug.utils import secure_filename
 from flask_cors import CORS, cross_origin
 import logging
 
 
 UPLOAD_FOLDER = '../assets/'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -19,16 +18,15 @@ def healthcheck():
 
 @app.route('/api/submit', methods=['POST'])
 def post_video():
-    target = os.path.join(UPLOAD_FOLDER, 'test_docs')
+    target = os.path.join(UPLOAD_FOLDER, './')
     if not os.path.isdir(target):
         os.mkdir(target)
-    file = request.files['file']
-    filename = secure_filename(file.filename)
+    vid_file = request.files['file']
+    filename = secure_filename(vid_file.filename)
     destination = "/".join([target, filename])
-    file.save(destination)
+    vid_file.save(destination)
     session['uploadFilePath'] = destination
-    response = "Whatever you wish too return"
-    return response
+    return jsonify({"res": "Video uploaded successfully"})
 
 
 app.secret_key = os.urandom(24)
