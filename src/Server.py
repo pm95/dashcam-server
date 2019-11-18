d70daf21c05787ecdddb946c7253fd02a72560eb
Pietro Malky <pietromalky@gmail.com>
2019-11-18 11:30:18 -0600

    added cross_origin decorator

--- src/Server.py
+++ src/Server.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, session, jsonify
+import time
+from flask import Flask, request, session, jsonify
 from werkzeug.utils import secure_filename
 from flask_cors import CORS, cross_origin
-import logging
 
 
 UPLOAD_FOLDER = '../assets/'
@@ -16,7 +16,23 @@ def healthcheck():
     return "Server is Healthy"
 
 
+@app.route('/api/getuserdata', methods=['GET'])
+@cross_origin()
+def get_user_data():
+    user_data = {
+        "firstName": "Pietro",
+        "lastName": "Test",
+        "carMake": "Toyota",
+        "carModel": "4Runner",
+        "plan": "Free",
+        "capacity": "100 GB",
+        "available": "100 GB"
+    }
+    return jsonify(user_data)
+
+
 @app.route('/api/submit', methods=['POST'])
+@cross_origin()
 def post_video():
     target = os.path.join(UPLOAD_FOLDER, './')
     if not os.path.isdir(target):
@@ -26,7 +42,7 @@ def post_video():
     destination = "/".join([target, filename])
     vid_file.save(destination)
     session['uploadFilePath'] = destination
-    return jsonify({"res": "Video uploaded successfully"})
+    return "Video uploaded successfully"
 
 
 app.secret_key = os.urandom(24)
