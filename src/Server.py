2a35dffff40039655b6f2a3a5f1f9f82980aa685
Pietro Malky <pietromalky@gmail.com>
2019-11-20 23:11:46 -0600

    routes are getting good progress. need to add route to handle user history

--- src/Server.py
+++ src/Server.py
@@ -24,13 +24,14 @@ def healthcheck():
 
 
 # new routes
-@app.route('/api/login', methods=['GET'])
+@app.route('/api/login', methods=['POST'])
 @cross_origin()
 def user_login():
-    data = request.get_json()
+    email = (request.data).decode('utf-8')
     conn = sqlite3.connect(CONFIG.DB_PATH)
     c = conn.cursor()
-    args = (data["email"],)
+    args = (email,)
+    print(email)
     c.execute("SELECT * FROM USERS WHERE email=?", args)
     match = c.fetchone()
     print(match)
@@ -41,7 +42,12 @@ def user_login():
             "email": match[2],
             "plan": match[3],
         })
-    return jsonify("user not found")
+    return jsonify({
+        "firstName": "NOT FOUND",
+        "lastName": "NOT FOUND",
+        "email": "NOT FOUND",
+        "plan": "NOT FOUND",
+    })
 
 
 @app.route('/api/signup', methods=['POST'])
@@ -71,34 +77,65 @@ def create_user():
     return response
 
 
-# routes in progress
 @app.route('/api/submit', methods=['POST'])
 @cross_origin()
 def post_video():
-    # target = os.path.join(UPLOAD_FOLDER, './')
-    # if not os.path.isdir(target):
-    #     os.mkdir(target)
-    # vid_file = request.files['file']
-    # filename = secure_filename(vid_file.filename)
-    # destination = "/".join([target, filename])
-    # vid_file.save(destination)
-    # session['uploadFilePath'] = destination
-    # return "Video uploaded successfully"
+    form_data = request.form
 
-    # old routes
+    videoName = form_data.get("videoName")
+    email = form_data.get("email")
+
+    conn = sqlite3.connect(CONFIG.DB_PATH)
+    c = conn.cursor()
+    args = (videoName, email,)
+    c.execute("SELECT * FROM VIDEOS WHERE videoName=? and email=?", args)
+    matches = c.fetchall()
+    if matches == []:
+        # insert new record into database with user email + video name
+        print("inserted new value into database")
+        c.execute("INSERT INTO VIDEOS (videoName, email) VALUES (?, ?)", args)
+        conn.commit()
+
+        # save video file to disk
+        target = os.path.join(UPLOAD_FOLDER, './')
+        if not os.path.isdir(target):
+            os.mkdir(target)
+        vid_file = request.files['file']
+        filename = secure_filename(vid_file.filename)
+        destination = "/".join([target, filename])
+        vid_file.save(destination)
+        session['uploadFilePath'] = destination
+        response = "success"
+    else:
+        print("videoName + email combination already exists")
+        response = "failure"
+    c.close()
+    return response
 
 
 @app.route('/api/updateuser', methods=['POST'])
 @cross_origin()
 def get_user_data():
-    data = request.get_json()
-    conn = sqlite3.connect(CONFIG.DB_PATH)
-    c = conn.cursor()
-    args = (data["email"],)
-    c.execute("SELECT * FROM USERS WHERE email=?", args)
-    match = c.fetchone()
-    print(match)
-    return "updateuser end hit"
+    data = request.get_json(force=True)
+    print(data)
+    try:
+        conn = sqlite3.connect(CONFIG.DB_PATH)
+        c = conn.cursor()
+        args = (
+            data["firstName"],
+            data["lastName"],
+            data["plan"],
+            data["email"],
+        )
+        c.execute(
+            "UPDATE USERS SET firstName=?, lastName=?, plan=? WHERE email=?",
+            args
+        )
+        conn.commit()
+        c.close()
+        return "Updated your information"
+    except:
+        return "Could not update your information right now"
 
 
 app.secret_key = os.urandom(24)
